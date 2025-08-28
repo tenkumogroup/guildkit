074e9e9dacc4899cc4667ceb8f25e024c5b95fd4
Jumpei Ogawa <git@phanective.org>
2025-08-28 18:51:41 +0900

    style: lint

--- projects/website/astro.config.ts
+++ projects/website/astro.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import compress from 'astro-compress'
-import icon from 'astro-icon'
-import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "astro/config";
+import compress from "astro-compress";
+import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  site: 'https://accessible-astro-starter.incluud.dev',
-  integrations: [mdx(), icon(), compress()],
+  site: "https://accessible-astro-starter.incluud.dev",
+  integrations: [
+    mdx(),
+    icon(),
+    compress(),
+  ],
   vite: {
     css: {
       preprocessorOptions: {
@@ -20,18 +23,20 @@ export default defineConfig({
         },
       },
     },
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+    ],
     resolve: {
       alias: {
-        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
-        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-        '@content': fileURLToPath(new URL('./src/content', import.meta.url)),
-        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
-        '@public': fileURLToPath(new URL('./public', import.meta.url)),
-        '@post-images': fileURLToPath(new URL('./public/posts', import.meta.url)),
-        '@project-images': fileURLToPath(new URL('./public/projects', import.meta.url)),
+        "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+        "@content": fileURLToPath(new URL("./src/content", import.meta.url)),
+        "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+        "@public": fileURLToPath(new URL("./public", import.meta.url)),
+        "@post-images": fileURLToPath(new URL("./public/posts", import.meta.url)),
+        "@project-images": fileURLToPath(new URL("./public/projects", import.meta.url)),
       },
     },
   },
-})
+});
