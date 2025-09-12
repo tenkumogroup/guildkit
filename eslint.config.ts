5b38fdc784bf9d8aaef74f776818fc6efa9c940a
Jumpei Ogawa <git@phanective.org>
2025-09-12 13:35:31 +0900

    wip: storybook

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig } from "eslint/config";
 import { core } from "@phanect/lint";
 import { nextjs } from "@phanect/lint-react";
