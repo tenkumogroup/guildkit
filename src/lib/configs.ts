6aa3049e6ac6ebb72737768786d45188ea22800d
Jumpei Ogawa <git@phanective.org>
2026-04-11 16:04:49 +0900

    feat: configure bucket name & Cloudflare account ID by environment variables

--- src/lib/configs.ts
+++ src/lib/configs.ts
@@ -5,12 +5,6 @@ type StorageConfig<PLATFORM extends "generic" | "aws" | "cloudflare" | "developm
   /** Bucket platform */
   platform: PLATFORM;
 
-  /** Bucket name. Default to "guildkit" */
-  bucket?: string;
-
-  /** Cloudflare account ID. Only required for Cloudflare R2. */
-  accountId?: PLATFORM extends "cloudflare" ? string | undefined : undefined;
-
   /** S3 client configs passing to `@aws-sdk/client-s3` */
   s3: PLATFORM extends "cloudflare" ? Omit<S3ClientConfig, "region"> & {
     /** @deprecated `region` is always `auto` on Cloudflare R2. */
