3afeb80640c565b752fb4270288e02c5d89d37cf
Jumpei Ogawa <git@phanective.org>
2026-04-11 13:00:10 +0900

    refactor: config type code

--- projects/guildkit/src/lib/configs.ts
+++ projects/guildkit/src/lib/configs.ts
@@ -1,55 +1,27 @@
 import userConfig from "../../../../guildkit.config.ts";
 import type { S3ClientConfig } from "@aws-sdk/client-s3";
 
-type BaseStorageConfig = S3ClientConfig & {
+type StorageConfig<PLATFORM extends "generic" | "aws" | "cloudflare" | "development" = "generic"> = {
+  /** Bucket platform */
+  platform: PLATFORM;
+
   /** Bucket name. Default to "guildkit" */
   bucket?: string;
 
-  // Unconfigurable by default
-  /** @deprecated This parameter is only required for Cloudflare R2 and ignored for this platform. */
-  accountId?: undefined;
-};
+  /** Cloudflare account ID. Only required for Cloudflare R2. */
+  accountId?: PLATFORM extends "cloudflare" ? string | undefined : undefined;
 
-type CloudflareR2Config = Omit<BaseStorageConfig, "accountId" | "region"> & {
-  platform: "cloudflare";
-  accountId?: string;
-
-  // Unconfigurable parameters
-  /** @deprecated `region` is always `auto` on Cloudflare R2. */
-  region?: "auto";
-};
-
-type MinioConfig = Omit<BaseStorageConfig, "region" | "forcePathStyle"> & {
-  platform: "minio";
-
-  // Unconfigurable parameters
-  /** @deprecated This parameter is ignored. `region` is always `us-east-1` for Min.io. */
-  region?: "us-east-1";
-  /** @deprecated This parameter is ignored. `forcePathStyle` is always `true` for Min.io. */
-  forcePathStyle?: true;
-};
-
-type RustfsConfig = Omit<BaseStorageConfig, "region" | "forcePathStyle"> & {
-  platform: "rustfs";
-
-  // Unconfigurable parameters
-  /** @deprecated This parameter is ignored. `region` is always `us-east-1` for rustfs. */
-  region?: "us-east-1";
-  /** @deprecated This parameter is ignored. `forcePathStyle` is always `true` for rustfs. */
-  forcePathStyle?: true;
-};
-
-type DevStorageConfig = Omit<MinioConfig, "platform" | "endpoint" | "credentials"> & {
-  platform: "development";
-};
-
-type AwsS3OrCustomConfig = BaseStorageConfig & {
-  platform: "aws" | "custom";
+  /** S3 client configs passing to `@aws-sdk/client-s3` */
+  s3: PLATFORM extends "cloudflare" ? Omit<S3ClientConfig, "region"> & {
+    /** @deprecated `region` is always `auto` on Cloudflare R2. */
+    region?: "auto";
+  } : PLATFORM extends "development" ? Omit<S3ClientConfig, "endpoint" | "region" | "forcePathStyle" | "credentials">
+  : S3ClientConfig;
 };
 
 export type GuildKitConfig = {
   siteName: string;
-  storage: CloudflareR2Config | MinioConfig | RustfsConfig | DevStorageConfig | AwsS3OrCustomConfig;
+  storage: StorageConfig;
   maxLogoSizeMiB?: number;
 };
 
